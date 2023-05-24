a88470c0b7c6d98feab5a1333f3d66f5fd5d64f9
babacardioume <128488983+babacardioume@users.noreply.github.com>
2023-05-24 01:33:57 +0000

    ¨Projet¨

---
+++ app/src/main/java/com/example/myblog/AjoutBlog.kt
@@ -0,0 +1,35 @@
+package com.example.myblog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+
+class AjoutBlog : AppCompatActivity() {
+    private lateinit var titre: EditText
+    private lateinit var description: EditText
+    private lateinit var add_button: Button
+    private lateinit var close: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nouveau_blog)
+
+        titre = findViewById(R.id.titre)
+        description = findViewById(R.id.contenu)
+        add_button = findViewById(R.id.add_button)
+        close = findViewById(R.id.button)
+
+        close.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        add_button.setOnClickListener {
+            val mydb = DBSQLiteHelper(this)
+            mydb.addArticle(titre.text.toString().trim(), description.text.toString().trim())
+        }
+    }
+}
